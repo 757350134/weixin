e89a0c008e2d4874ae9f39641008c102da54006c
wxp <757350134@qq.com>
2016-12-09 11:11:28 +0800

    Create wxp.java

---
+++ wxp.java
@@ -0,0 +1,10 @@
+class wxp{
+  private  int  age;
+  private String name;
+  public wxp(int age,String name){
+  this.age=age;
+  this.name=name;
+  }
+  public void static main(String args[])
+  new wxp(19,"xiaoming"); 
+ }
